7e0d350cfe64446a596a27563a693892d13139ff
harshita patil <harshita.patil1992@gmail.com>
2023-12-03 11:46:47 -0800

    create Invalid_tweets.py

---
+++ Invalid_tweets.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+def invalid_tweets(tweets: pd.DataFrame) -> pd.DataFrame:
+    is_valid = tweets['content'].str.len()>15
+    df = tweets[is_valid]
+    return df[['tweet_id']]
+    
